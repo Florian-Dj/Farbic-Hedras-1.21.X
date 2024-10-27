cd10c0ee0bd2c3f82de7c9b9b5f4ec5eb845e9be
DJERBI <floriandjerbi@gmail.com>
2024-10-27 21:42:57 +0100

    Initial commit

---
+++ src/main/java/net/hedras/HedrasMod.java
@@ -0,0 +1,15 @@
+package net.hedras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HedrasMod implements ModInitializer {
+	public static final String MOD_ID = "hedrasmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/hedras/HedrasModClient.java
@@ -0,0 +1,10 @@
+package net.hedras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HedrasModClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
